d1505809393efc61799acfa85b2bdf4e8db9880f
hakantasan <hakantasan5@gmail.com>
2015-08-25 03:38:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form id="form" action="veriler.php" method="get">
+            Adı: <input type="text" id="ad" name="ad">
+            <br><br>
+            Soyadı: <input type="text" id="soyad" name="soyad">
+            <br><br>
+            <button type="submit" id="gonder">Gönder</button>
+        </form>
+    </body>
+</html>
